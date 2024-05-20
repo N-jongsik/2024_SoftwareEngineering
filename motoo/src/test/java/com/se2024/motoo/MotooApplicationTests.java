5083b5999ab49a90b1bde2e89bff4b90caea934e
178kg78cm <101550897+178kg78cm@users.noreply.github.com>
2024-05-21 01:21:43 +0900

    init

---
+++ motoo/src/test/java/com/se2024/motoo/MotooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se2024.motoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
