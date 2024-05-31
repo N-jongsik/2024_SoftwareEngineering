7c37b81b19e965cfbe62ffafebf27c97963576c3
syoooooung <tpdudabc@naver.com>
2024-06-01 02:14:53 +0900

    beforechecking

--- motoo/src/main/java/com/se2024/motoo/MotooApplication.java
+++ motoo/src/main/java/com/se2024/motoo/MotooApplication.java
@@ -2,8 +2,10 @@ package com.se2024.motoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MotooApplication {
 
 	public static void main(String[] args) {
