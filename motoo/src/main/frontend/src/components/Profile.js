d3dbbf56e777f3069a8d3c4f72224b29b0d5eacb
seong hyeon Jeong <swjsh19@gmail.com>
2024-06-16 17:49:21 +0900

    24.06.16

--- motoo/src/main/frontend/src/components/Profile.js
+++ motoo/src/main/frontend/src/components/Profile.js
@@ -3,42 +3,43 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
-  const [user, setUser] = useState(null);
-  const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    const checkSession = async () => {
-      try {
-        const response = await axios.get('/api/me');
-        if (response.data.status === 'success') {
-          setUser(response.data.user);  // 사용자 정보를 상태에 저장
-        } else {
-          alert('로그인을 해주세요!');
-          navigate('/login');
-        }
-      } catch (error) {
-        console.error('Session check error', error);
-        navigate('/login');
-      }
-    };
+    useEffect(() => {
+        const checkSession = async () => {
+            try {
+                const response = await axios.get('/api/me');
+                if (response.data.status === 'success') {
+                    setUser(response.data.user);  // 사용자 정보를 상태에 저장
+                } else {
+                    alert('로그인을 해주세요!');
+                    navigate('/login');
+                }
+            } catch (error) {
+                console.error('Session check error', error);
+                navigate('/login');
+            }
+        };
 
-    checkSession();
-  }, [navigate]);
+        checkSession();
+    }, [navigate]);
 
-  if (!user) {
-    return <div>Loading...</div>;  // 사용자 정보를 로드 중일 때 로딩 표시
-  }
+    if (!user) {
+        return <div>Loading...</div>;  // 사용자 정보를 로드 중일 때 로딩 표시
+    }
 
-  return (
-    <div>
-      <h1>개인 정보 확인하기</h1>
-      <h2>사용자 이름: {user.userName}</h2>  {/* 사용자의 이름을 출력 */}
-      <h2>사용자 ID: {user.userID}</h2>  {/* 사용자의 ID를 출력 */}
-      <h2>사용자 Email: {user.userEmail}</h2>  {/* 사용자의 이메일을 출력 */}
-      <h2>Motoo의 {user.id}번째 회원입니다!</h2>
-      {/* 필요한 다른 사용자 정보 추가 */}
-    </div>
-  );
+    return (
+        <div className="dashboard-container">
+            <p><b>Motoo의 {user.id}번째 회원입니다!</b></p>
+            <h1>개인 정보 확인하기</h1>
+            <h2>사용자 이름: {user.userName}</h2>  {/* 사용자의 이름을 출력 */}
+            <h2>사용자 ID: {user.userID}</h2>  {/* 사용자의 ID를 출력 */}
+            <h2>사용자 Email: {user.userEmail}</h2>  {/* 사용자의 이메일을 출력 */}
+
+            {/* 필요한 다른 사용자 정보 추가 */}
+        </div>
+    );
 };
 
 export default Dashboard;
